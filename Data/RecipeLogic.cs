de8ab73d57b2dba9f347dc4c406825f6539b9f37
MihailBaldzhiev <mihailbaldzhiev@gmail.com>
2023-03-21 00:03:32 +0200

    added context, models and basic but unfinished logic

---
+++ Data/RecipeLogic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Recipe.Models;
+using Recipe.Data;
+
+namespace Recipe.Data
+{
+    public class RecipeLogic
+    {
+        private RecipeContext recipeContext;
+
+        public RecipeLogic(RecipeContext recipeContext)
+        {
+            this.recipeContext = recipeContext;
+        }
+
+        public List<RecipeModel> GetAll()
+        {
+            return recipeContext.Recipes.ToList();
+        }
+
+        public void Add(RecipeModel recipe)
+        {
+            recipeContext.Recipes.Add(recipe);
+            recipeContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var item = recipeContext.Recipes.Find(id);
+            if (item != null)
+            {
+                recipeContext.Recipes.Remove(item);
+                recipeContext.SaveChanges();
+            }
+        }
+    }
+}
